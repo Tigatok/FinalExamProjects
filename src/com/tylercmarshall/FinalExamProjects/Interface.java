bb79a2c87cefdc7c7d7d55be6aec0ec946b1bd9a
U-Tyler-PC\Tyler <tymarshall1@hotmail.com>
2014-04-12 16:18:07 -0700

    Added button locations

--- src/com/tylercmarshall/FinalExamProjects/Interface.java
+++ src/com/tylercmarshall/FinalExamProjects/Interface.java
@@ -1,5 +1,6 @@
 package com.tylercmarshall.FinalExamProjects;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -12,29 +13,105 @@ public class Interface extends JPanel implements ActionListener {
 
     private static final long serialVersionUID = 42L;
 
-    //Declaring the Radio Buttons for the selection
+    //Declaring the JRadioButtons for the selection
     private JRadioButton guiRB;
     private JRadioButton linkedListRB;
     private JRadioButton fileRB;
     private JRadioButton listenersRB;
     private JRadioButton recursiveRB;
     private JRadioButton stackRB;
-    //End RadioButtons
+    //End JRadioButtons
 
-    //Instantiates the Buttons
-    private JButton confirmSelection = new JButton("Confirm");
-    //End Buttons
-
-
+    //Declares the JButtons
+    private JButton confirmSelection;
+    //End JButtons
 
 
+    /**
+     * interface
+     *
+     * @description Loads the interface settings
+     */
+    public Interface() {
+
+        //Sets the layout to BorderLayout
+        setLayout(new BorderLayout());
+        //End Layout Settings
+
+        //JPanel instantiations
+        JPanel centerPanel = new JPanel();
+        JPanel northPanel = new JPanel();
+        JPanel southPanel = new JPanel();
+        JPanel eastPanel = new JPanel();
+        JPanel westPanel = new JPanel();
+        //End JPanels
+
+        //JButtonGroup Instantiations
+        ButtonGroup buttonGroup1 = new ButtonGroup();
+        //End JButtonGroups
+
+        //JRadioButton Instantiations
+        guiRB = new JRadioButton("Gui Example");
+        linkedListRB = new JRadioButton("Linked List Example");
+        fileRB = new JRadioButton("File I/O Example");
+        listenersRB = new JRadioButton("Listeners Example");
+        recursiveRB = new JRadioButton("Recursive Examples");
+        stackRB = new JRadioButton("Stack Examples");
+        //End JRadioButtons Instantiations
+
+        //JButton Instantiations
+        confirmSelection = new JButton("Confirm");
+        //End Button instantiations
 
 
+        //centerPanel settings
+        centerPanel.setLayout(new GridLayout(2, 3));
+        //End centerPanel settings
+
+        //Adding JRadioButtons to buttonGroup1
+        buttonGroup1.add(guiRB);
+        buttonGroup1.add(linkedListRB);
+        buttonGroup1.add(fileRB);
+        buttonGroup1.add(listenersRB);
+        buttonGroup1.add(recursiveRB);
+        buttonGroup1.add(stackRB);
+        //End Adding
+
+        //Adding JRadioButtons to centerPanel
+        centerPanel.add(guiRB);
+        centerPanel.add(linkedListRB);
+        centerPanel.add(fileRB);
+        centerPanel.add(linkedListRB);
+        centerPanel.add(listenersRB);
+        centerPanel.add(recursiveRB);
+        centerPanel.add(stackRB);
+        //End Adding
+
+        //Adding JButtons to southPanel
+        southPanel.add(confirmSelection);
+        //End Adding
+
+        //Adding JPanels to interface
+        add(centerPanel, BorderLayout.CENTER);
+        add(southPanel, BorderLayout.SOUTH);
+        //End Adding
+
+    }//End Interface Constructor
+
+
+    /**
+     *
+     * @description ActionPerformed method
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
 
+    /**
+     *main
+     *  @description Creates the JFrame
+     */
     public static void main(String[] args) {
 
         //Instantiates a new JFrame object
@@ -54,5 +131,6 @@ public class Interface extends JPanel implements ActionListener {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         //End Frame settings
-    }
+
+    }//End main
 }
