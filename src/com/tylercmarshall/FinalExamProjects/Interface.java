6116f0f0e94ea0664bd0db030f1d7765caec5239
Tyler <tymarshall1@hotmail.com>
2014-04-12 15:28:02 -0700

    Added layout

---
+++ src/com/tylercmarshall/FinalExamProjects/Interface.java
@@ -0,0 +1,19 @@
+package com.tylercmarshall.FinalExamProjects;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ * Created by Tyler on 4/12/2014.
+ */
+public class Interface extends JPanel implements ActionListener {
+
+    private static final long serialVersionUID = 42L;
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
